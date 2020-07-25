eb15068ff19d9c4cbd4ddc1734a8fdb0d9ee1fdb
Prakash <rssprakash99@gmail.com>
2020-07-25 10:55:27 +0530

    This is my first commit

---
+++ src/test/java/one/Prakash/JavaDemo.java
@@ -0,0 +1,10 @@
+package one.Prakash;
+
+public class JavaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/two/Prakash/PythonDemo.java
@@ -0,0 +1,10 @@
+package two.Prakash;
+
+public class PythonDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
